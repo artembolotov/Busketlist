4cd9e955863aea9da3b40aa7bbb341d13e6fa698
Artem Bolotov <teeemon@gmail.com>
2023-03-14 07:07:43 +0300

    Initial Commit

---
+++ Busketlist/BusketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusketlistApp.swift
+//  Busketlist
+//
+//  Created by artembolotov on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct BusketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
