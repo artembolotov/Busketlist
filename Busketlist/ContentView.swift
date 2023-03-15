e88a5054d7d69014c5cb95e9e7968d5d9bd7f0e2
Artem Bolotov <teeemon@gmail.com>
2023-03-15 08:05:33 +0300

    Face ID / Touch ID

--- Busketlist/ContentView.swift
+++ Busketlist/ContentView.swift
@@ -5,48 +5,39 @@
 //  Created by artembolotov on 14.03.2023.
 //
 
-import MapKit
 import SwiftUI
-
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let ccordinate: CLLocationCoordinate2D
-}
+import LocalAuthentication
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
-    
-    let locations = [
-        Location(
-            name: "Buckingham Palace",
-            ccordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)
-        ),
-        Location(
-            name: "Tower of London",
-            ccordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076)
-        )
-    ]
+    @State private var isUnlocked = false
     
     var body: some View {
-        NavigationView {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                MapAnnotation(coordinate: location.ccordinate) {
-                    
-                    NavigationLink {
-                        Text(location.name)
-                    } label: {
-                        Circle()
-                            .stroke(.red, lineWidth: 3)
-                            .frame(width: 33, height: 33)
-                    }
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            }  else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
                 }
             }
-            .edgesIgnoringSafeArea(.bottom)
-            .navigationTitle("London Explorer")
+        } else {
+            // no biometrics
         }
     }
 }
