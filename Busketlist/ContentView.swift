de93fe942cb4ae472fa6b8d3dcd3d5eec99a2531
Artem Bolotov <teeemon@gmail.com>
2023-03-15 07:41:04 +0300

    Maps added

--- Busketlist/ContentView.swift
+++ Busketlist/ContentView.swift
@@ -5,41 +5,48 @@
 //  Created by artembolotov on 14.03.2023.
 //
 
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-     case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading…")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed")
-    }
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let ccordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
-    var loadingState = LoadingState.loading
+    @State private var mapRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    let locations = [
+        Location(
+            name: "Buckingham Palace",
+            ccordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)
+        ),
+        Location(
+            name: "Tower of London",
+            ccordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076)
+        )
+    ]
     
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.ccordinate) {
+                    
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 33, height: 33)
+                    }
+                }
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            .navigationTitle("London Explorer")
         }
     }
 }
